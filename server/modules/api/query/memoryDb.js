90869e6df11b5ef56a01e3d8d876fe43de55d5d8
Janie Chen <janiec128@gmail.com>
2017-12-12 00:42:23 -0800

    Change in memory data variable to be class based

--- server/modules/api/query/memoryDb.js
+++ server/modules/api/query/memoryDb.js
@@ -1,4 +1,12 @@
 // In memory data
+export class Member {
+  constructor(firstName, lastName) {
+    this.id = Date.now()
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
+
 const theMemberList = [
   { id: Date.now(), firstName: 'First', lastName: 'User' }
 ]
@@ -8,12 +16,19 @@ export const getMemberList = () => theMemberList
 export const getMember = memberId => theMemberList.find(({ id }) => id === memberId)
 
 export const addToMemberList = ({ firstName, lastName }) => {
-  const newMember = { id: Date.now(), firstName, lastName }
+  const newMember = new Member(firstName, lastName)
   theMemberList.push(newMember)
   return newMember
 }
 
 // -----------------------
+export class Message {
+  constructor(channelID, message) {
+    this.id = Date.now()
+    this.channelID = channelID
+    this.message = message
+  }
+}
 
 const theMessageList = []
 
@@ -27,7 +42,7 @@ export const getMessageList = idFilter => {
 export const getMessage = messageId => theMessageList.find(({ id }) => id === messageId)
 
 export const addToMessageList = ({ channelID, message }) => {
-  const newMessage = { id: Date.now(), channelID, message }
+  const newMessage = new Message(channelID, message)
   theMessageList.push(newMessage)
   return newMessage
 }
